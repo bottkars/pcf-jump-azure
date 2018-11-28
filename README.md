e34ac097843f83703c7f5dfa65edfb745e91eabf
bottkars <karsten.bott@bottnet.de>
2018-11-28 20:54:12 +0100

    initial

---
+++ README.md
@@ -0,0 +1,80 @@
+# pcf-jump-azure
+
+## usage 
+create an .env file using the .env.example  
+the .env vile requires the following variables to be set:
+
+**IAAS**=*the iaas environment, azure*  
+**JUMPBOX_RG**=*the name of the ressource group for the JumpBox*  
+**JUMPBOX_NAME**=*the JumpBox hostname*   
+**ADMIN_USERNAME**=*ubuntu*  
+**AZURE_CLIENT_ID**=*fake your azure client id*  
+**AZURE_CLIENT_SECRET**=*fake your azure client secret*  
+**AZURE_REGION**=*westeurope*  
+**AZURE_SUBSCRIPTION_ID**=*fake your azure subscription id*  
+**AZURE_TENANT_ID**=*fake your azure tenant*  
+**PCF_PIVNET_UAA_TOKEN**=*fave your pivnet refresh token*  
+**OM_HOSTNAME**=*opsman.yourdomain.com*  
+**ENV_NAME**=*yourenv*  
+**ENV_SHORT_NAME**=*yourenvshort*  
+**OPS_MANAGER_IMAGE_URI**=*"https://opsmanagerwesteurope.blob.core.windows.net/images/ops-manager-2.3-build.194.vhd"*  
+**DNS_SUFFIX**=*yourdomain.com*  
+**DNS_SUBDOMAIN**=*yourpcf*  
+**PRODUCT_SLUG**=*elastic-runtime*  
+**RELEASE_ID**=*220833*  
+
+```bash
+source .env
+```
+## create a aah keypair for the admin user
+
+```bash
+ssh-keygen -t rsa -f ~/opsman -C ${ADMIN_USERNAME}
+```
+
+## create the target resource group for the jumpbox
+
+```bash
+az group create --name ${JUMPBOX_RG} --location ${AZURE_REGION}
+```
+
+## start the deployment
+
+```bash
+az group deployment create --resource-group ${JUMPBOX_RG} \
+    --template-uri https://raw.githubusercontent.com/bottkars/pcf-jump-azure/master/azuredeploy.json \
+    --parameters \
+    adminUsername=${ADMIN_USERNAME} \
+    sshKeyData="$(cat ~/opsman.pub)" \
+    sshKey="$(cat ~/opsman)" \
+    dnsLabelPrefix=${JUMPBOX_NAME} \
+    clientSecret=${AZURE_CLIENT_SECRET} \
+    clientID=${AZURE_CLIENT_ID} \
+    tenantID=${AZURE_TENANT_ID} \
+    subscriptionID=${AZURE_SUBSCRIPTION_ID} \
+    omHostname=${OM_HOSTNAME} \
+    pivnetToken=${PCF_PIVNET_UAA_TOKEN} \
+    env_name=${ENV_NAME} \
+    env_short_name=${ENV_SHORT_NAME} \
+    ops_manager_image_uri=${OPS_MANAGER_IMAGE_URI} \
+    dns_suffix=${DNS_SUFFIX} \
+    dns_subdomain=${DNS_SUBDOMAIN} \
+    adminUsername=${ADMIN_USERNAME} \
+    product_slug=${PRODUCT_SLUG} \
+    release_id=${RELEASE_ID}
+```
+
+```bash
+ ssh -i ~/opsman ubuntu@${JUMPBOX_NAME}.${AZURE_REGION}.cloudapp.azure.com
+```
+
+## cleanup
+
+```bash
+az group delete --name ${JUMPBOX_RG} --yes
+az group delete --name ${ENV_NAME} --yes
+ssh-keygen -R "${JUMPBOX_NAME}.${AZURE_REGION}.cloudapp.azure.com"
+```
+
+
+
---
+++ scripts/deploy_base.sh
@@ -0,0 +1,187 @@
+#!/usr/bin/env bash
+
+function retryop()
+{
+  retry=0
+  max_retries=10
+  interval=30
+  while [ ${retry} -lt ${max_retries} ]; do
+    echo "Operation: $1, Retry #${retry}"
+    eval $1
+    if [ $? -eq 0 ]; then
+      echo "Successful"
+      break
+    else
+      let retry=retry+1
+      echo "Sleep $interval seconds, then retry..."
+      sleep $interval
+    fi
+  done
+  if [ ${retry} -eq ${max_retries} ]; then
+    echo "Operation failed: $1"
+    exit 1
+  fi
+}
+
+echo "Installing jq"
+retryop "apt-get update && apt-get install -y jq"
+
+function get_setting() {
+  key=$1
+  local value=$(echo $settings | jq ".$key" -r)
+  echo $value
+}
+
+custom_data_file="/var/lib/cloud/instance/user-data.txt"
+settings=$(cat ${custom_data_file})
+ADMIN_USERNAME=$(get_setting ADMIN_USERNAME)
+AZURE_CLIENT_ID=$(get_setting AZURE_CLIENT_ID)
+AZURE_CLIENT_SECRET=$(get_setting AZURE_CLIENT_SECRET)
+AZURE_SUBSCRIPTION_ID=$(get_setting AZURE_SUBSCRIPTION_ID)
+AZURE_TENANT_ID=$(get_setting AZURE_TENANT_ID)
+PCF_PIVNET_UAA_TOKEN=$(get_setting PCF_PIVNET_UAA_TOKEN)
+OM_HOSTNAME=$(get_setting OM_HOSTNAME)
+ENV_NAME=$(get_setting ENV_NAME)
+ENV_SHORT_NAME=$(get_setting ENV_SHORT_NAME)
+OPS_MANAGER_IMAGE_URI=$(get_setting OPS_MANAGER_IMAGE_URI)
+LOCATION=$(get_setting LOCATION)
+DNS_SUFFIX=$(get_setting DNS_SUFFIX)
+DNS_SUBDOMAIN=$(get_setting DNS_SUBDOMAIN)
+PRODUCT_SLUG=$(get_setting PRODUCT_SLUG)
+RELEASE_ID=$(get_setting RELEASE_ID)
+SSH_PRIVATE_KEY=$(get_setting SSH_PRIVATE_KEY)
+SSH_PUBLIC_KEY=$(get_setting SSH_PUBLIC_KEY)
+
+
+HOME_DIR="/home/${ADMIN_USERNAME}"
+
+cp *.sh ${HOME_DIR}
+chown ${ADMIN_USERNAME}.${ADMIN_USERNAME} ${HOME_DIR}/*.sh
+chmod 755 ${HOME_DIR}/*.sh
+chmod +X ${HOME_DIR}/*.sh
+
+cat << EOF > ${HOME_DIR}/.env.sh
+#!/usr/bin/env bash
+ADMIN_USERNAME=${ADMIN_USERNAME}
+AZURE_CLIENT_SECRET=${AZURE_CLIENT_SECRET}
+AZURE_CLIENT_ID=$AZURE_CLIENT_ID}
+AZURE_TENANT_ID=${AZURE_TENANT_ID}
+AZURE_SUBSCRIPTION_ID=${AZURE_SUBSCRIPTION_ID}
+PCF_PIVNET_UAA_TOKEN=${PCF_PIVNET_UAA_TOKEN}
+OM_HOSTNAME=${OM_HOSTNAME}
+ENV_NAME=${ENV_NAME}
+ENV_SHORT_NAME=${ENV_SHORT_NAME}
+OPS_MANAGER_IMAGE_URI=${OPS_MANAGER_IMAGE_URI}
+LOCATION=${LOCATION}
+DNS_SUFFIX=${DNS_SUFFIX}
+DNS_SUBDOMAIN=${DNS_SUBDOMAIN}
+PRODUCT_SLUG=${PRODUCT_SLUG}
+RELEASE_ID=${RELEASE_ID}
+SSH_PRIVATE_KEY=${SSH_PRIVATE_KEY}
+SSH_PUBLIC_KEY=${SSH_PUBLIC_KEY}
+EOF
+
+chmod 600 ${HOME_DIR}/.env.sh
+chown ${ADMIN_USERNAME}.${ADMIN_USERNAME} ${HOME_DIR}/.env.sh
+
+cp * ${HOME_DIR}
+
+sudo apt-get install apt-transport-https lsb-release software-properties-common -y
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+
+sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
+     --keyserver packages.microsoft.com \
+     --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
+
+sudo apt-get update
+
+sudo apt-get install azure-cli && sudo apt --yes install unzip 
+
+wget -O terraform.zip https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_linux_amd64.zip && \
+  unzip terraform.zip && \
+  sudo mv terraform /usr/local/bin
+
+wget -O om https://github.com/pivotal-cf/om/releases/download/0.44.0/om-linux && \
+  chmod +x om && \
+  sudo mv om /usr/local/bin/
+
+wget -O bosh https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-5.3.1-linux-amd64 && \
+  chmod +x bosh && \
+  sudo mv bosh /usr/local/bin/
+
+wget -O /tmp/bbr.tar https://github.com/cloudfoundry-incubator/bosh-backup-and-restore/releases/download/v1.2.8/bbr-1.2.8.tar && \
+  tar xvC /tmp/ -f /tmp/bbr.tar && \
+  sudo mv /tmp/releases/bbr /usr/local/bin/
+# get pivnet UAA TOKEN
+
+cd ${HOME_DIR}
+
+AUTHENTICATION_RESPONSE=$(curl \
+  --fail \
+  --data "{\"refresh_token\": \"${PCF_PIVNET_UAA_TOKEN}\"}" \
+  https://network.pivotal.io/api/v2/authentication/access_tokens)
+
+PIVNET_ACCESS_TOKEN=$(echo ${AUTHENTICATION_RESPONSE} | jq -r '.access_token')
+# Get the release JSON for the PAS version you want to install:
+
+RELEASE_JSON=$(curl \
+    --fail \
+    "https://network.pivotal.io/api/v2/products/${PRODUCT_SLUG}/releases/${RELEASE_ID}")
+
+# ACCEPTING EULA
+
+EULA_ACCEPTANCE_URL=$(echo ${RELEASE_JSON} |\
+  jq -r '._links.eula_acceptance.href')
+
+curl \
+  --fail \
+  --header "Authorization: Bearer ${PIVNET_ACCESS_TOKEN}" \
+  --request POST \
+  ${EULA_ACCEPTANCE_URL}
+
+# GET TERRAFORM FOR PCF AZURE
+
+DOWNLOAD_ELEMENT=$(echo ${RELEASE_JSON} |\
+  jq -r '.product_files[] | select(.aws_object_key | contains("terraforming-azure"))')
+
+FILENAME=$(echo ${DOWNLOAD_ELEMENT} |\
+  jq -r '.aws_object_key | split("/") | last')
+
+URL=$(echo ${DOWNLOAD_ELEMENT} |\
+  jq -r '._links.download.href')
+
+# download terraform
+
+curl \
+  --fail \
+  --location \
+  --output ${FILENAME} \
+  --header "Authorization: Bearer ${PIVNET_ACCESS_TOKEN}" \
+  ${URL}
+sudo -S -u ubuntu unzip ${FILENAME}
+cd ./pivotal-cf-terraforming-azure-*/
+cd terraforming-pas
+
+ # preparation work for terraform
+cat << EOF > terraform.tfvars
+client_id             = "${AZURE_CLIENT_ID}"
+client_secret         = "${AZURE_CLIENT_SECRET}"
+subscription_id       = "${AZURE_SUBSCRIPTION_ID}"
+tenant_id             = "${AZURE_TENANT_ID}"
+env_name              = "${ENV_NAME}"
+env_short_name        = "${ENV_SHORT_NAME}"
+ops_manager_image_uri = "${OPS_MANAGER_IMAGE_URI}"
+location              = "${LOCATION}"
+dns_suffix            = "${DNS_SUFFIX}"
+dns_subdomain         = "${DNS_SUBDOMAIN}"
+EOF
+
+chmod 755 terraform.tfvars
+chown ${ADMIN_USERNAME}.${ADMIN_USERNAME} terraform.tfvars
+sudo -S -u ubuntu terraform init
+sudo -S -u ubuntu terraform plan -out=plan
+sudo -S -u ubuntu terraform apply -auto-approve
+
+
