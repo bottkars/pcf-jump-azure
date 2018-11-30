a77b5bee08e12ae690530c59cdef477de1f357a8
bottkars <Karsten.Bott@bottnet.de>
2018-11-30 06:52:08 +0100

    latest

--- scripts/deploy_pas.sh
+++ scripts/deploy_pas.sh
@@ -6,7 +6,7 @@ PCF_KEY_PEM=$(cat ${PCF_SUBDOMAIN_NAME}.${PCF_DOMAIN_NAME}.key | awk '{printf "%
 PCF_CERT_PEM=$(cat ${PCF_SUBDOMAIN_NAME}.${PCF_DOMAIN_NAME}.cert | awk '{printf "%s\\r\\n", $0}')
 PCF_CREDHUB_KEY="01234567890123456789"
 PRODUCT_NAME=cf
-PCF_PAS_NETWORK="pas.${PCF_SUBDOMAIN_NAME}.${PCF_DOMAIN_NAME}"
+PCF_PAS_NETWORK="pcf-pas-subnet"
 PCF_APPS_DOMAIN="apps.${PCF_SUBDOMAIN_NAME}.${PCF_DOMAIN_NAME}"
 PCF_SYSTEM_DOMAIN="sys.${PCF_SUBDOMAIN_NAME}.${PCF_DOMAIN_NAME}"
 PCF_WEB_LB="${ENV_NAME}-web-lb"
@@ -67,7 +67,7 @@ curl \
 # Import the tile to Ops Manager.
 
 om \
-  --username ${PCF_OPSMAN_USER} \
+  --username ${PCF_OPSMAN_USERNAME} \
   --password ${PCF_PIVNET_UAA_TOKEN} \
   --target ${PCF_OPSMAN_FQDN} \
   --skip-ssl-validation \
@@ -76,7 +76,7 @@ om \
 
     # 1. Find the version of the product that was imported.
 PRODUCTS=$(om \
-  --username ${PCF_OPSMAN_USER} \
+  --username ${PCF_OPSMAN_USERNAME} \
   --password ${PCF_PIVNET_UAA_TOKEN} \
   --target ${PCF_OPSMAN_FQDN} \
   --skip-ssl-validation \
@@ -89,7 +89,7 @@ VERSION=$(echo ${PRODUCTS} |\
 # 2.  Stage using om cli
 
 om \
-  --username ${PCF_OPSMAN_USER} \
+  --username ${PCF_OPSMAN_USERNAME} \
   --password ${PCF_PIVNET_UAA_TOKEN} \
   --target ${PCF_OPSMAN_FQDN} \
   --skip-ssl-validation \
@@ -105,13 +105,13 @@ pcf_notifications_email: ${PCF_NOTIFICATIONS_EMAIL}
 pcf_cert_pem: "${PCF_CERT_PEM}"
 pcf_key_pem: "${PCF_KEY_PEM}"
 pcf_credhub_key: "${PCF_CREDHUB_KEY}"
-pcf_diego_ssh_lb: {$PCF_DIEGO_SSH_LB}
-pcf_mysql_lb: {$PCF_MYSQL_LB}
-pcf_web_lb: {$PCF_WEB_LB}
+pcf_diego_ssh_lb: ${PCF_DIEGO_SSH_LB}
+pcf_mysql_lb: ${PCF_MYSQL_LB}
+pcf_web_lb: ${PCF_WEB_LB}
 EOF
 
 om \
-  --username ${PCF_OPSMAN_USER} \
+  --username ${PCF_OPSMAN_USERNAME} \
   --password ${PCF_PIVNET_UAA_TOKEN} \
   --target ${PCF_OPSMAN_FQDN} \
   --skip-ssl-validation \
@@ -120,7 +120,7 @@ om \
     --product-name ${PRODUCT_NAME}
 ###
 om \
-  --username ${PCF_OPSMAN_USER} \
+  --username ${PCF_OPSMAN_USERNAME} \
   --password ${PCF_PIVNET_UAA_TOKEN} \
   --target ${PCF_OPSMAN_FQDN} \
   --skip-ssl-validation \
