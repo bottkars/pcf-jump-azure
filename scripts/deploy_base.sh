90bfae59dd18434e6e70879e2bd6fccedfb2d145
bottkars <karsten.bott@bottnet.de>
2019-01-11 12:09:50 +0100

    typo

--- scripts/deploy_base.sh
+++ scripts/deploy_base.sh
@@ -101,7 +101,7 @@ PCF_PAS_VERSION="${PCF_PAS_VERSION}"
 NET_16_BIT_MASK="${NET_16_BIT_MASK}"
 START_BASE_DEPLOY_TIME="${START_BASE_DEPLOY_TIME}"
 DOWNLOAD_DIR="${DOWNLOAD_DIR}"
-SMTP_ADDRESS="${SMTP_ADDRESS}""
+SMTP_ADDRESS="${SMTP_ADDRESS}"
 SMTP_IDENTITY="${SMTP_IDENTITY}"
 SMTP_PASSWORD="${SMTP_PASSWORD}"
 SMTP_FROM="${SMTP_FROM}"
@@ -115,7 +115,7 @@ chown ${ADMIN_USERNAME}.${ADMIN_USERNAME} ${HOME_DIR}/.env.sh
 
 cp * ${HOME_DIR}
 
-sudo apt-get install apt-transport-https lsb-release software-properties-common ruby ruby-dev gcc libffi-dev make build-essential -y
+sudo apt-get install apt-transport-https lsb-release software-properties-common -y
 AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
     sudo tee /etc/apt/sources.list.d/azure-cli.list
@@ -144,7 +144,6 @@ wget -O /tmp/bbr.tar https://github.com/cloudfoundry-incubator/bosh-backup-and-r
   tar xvC /tmp/ -f /tmp/bbr.tar && \
   sudo mv /tmp/releases/bbr /usr/local/bin/
 # get pivnet UAA TOKEN
-gem install cf-uaac
 
 cd ${HOME_DIR}
 
@@ -218,10 +217,10 @@ chmod 755 terraform.tfvars
 chown ${ADMIN_USERNAME}.${ADMIN_USERNAME} terraform.tfvars
 sudo -S -u ubuntu terraform init
 sudo -S -u ubuntu terraform plan -out=plan
-retryop "sudo -S -u ubuntu terraform apply -auto-approve" 3 1
+retryop "sudo -S -u ubuntu terraform apply -auto-approve" 3 10
 
-terraform output ops_manager_ssh_private_key > ~/.ssh/opsman
-chmod 600 ~/opsman
+sudo -S -u ubuntu terraform output ops_manager_ssh_private_key > ${HOME_DIR}/opsman
+sudo -S -u ubuntu chmod 600 ${HOME_DIR}/opsman
 
 
 END_BASE_DEPLOY_TIME=$(date)
