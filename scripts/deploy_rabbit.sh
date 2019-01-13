0706ffb4c554def8280ffe04eb760f7374fb6632
bottkars <karsten.bott@bottnet.de>
2019-01-13 18:57:03 +0100

    rabbit prepare

--- scripts/deploy_rabbit.sh
+++ scripts/deploy_rabbit.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
-exec > >(tee -i -a ~/deploy_mysql.log)
+exec > >(tee -i -a ~/deploy_rabbit.log)
 exec 2>&1
 
 source ~/.env.sh
 export OM_TARGET=${PCF_OPSMAN_FQDN}
 export OM_USERNAME=${PCF_OPSMAN_USERNAME}
 export OM_PASSWORD="${PCF_PIVNET_UAA_TOKEN}"
-START_MYSQL_DEPLOY_TIME=$(date)
+START_RABBIT_DEPLOY_TIME=$(date)
 $(cat <<-EOF >> ${HOME_DIR}/.env.sh
-START_MYSQL_DEPLOY_TIME="${START_MYSQL_DEPLOY_TIME}"
+START_RABBIT_DEPLOY_TIME="${START_RABBIT_DEPLOY_TIME}"
 EOF
 )
 
-source  ~/mysql.env
-cd ${HOME_DIR}
+source  ~/rabbit.env
 
 PIVNET_ACCESS_TOKEN=$(curl \
   --fail \
@@ -30,7 +29,7 @@ RELEASE_JSON=$(curl \
 EULA_ACCEPTANCE_URL=$(echo ${RELEASE_JSON} |\
   jq -r '._links.eula_acceptance.href')
 
-DOWNLOAD_DIR_FULL=${DOWNLOAD_DIR}/${PRODUCT_SLUG}/${PCF_MYSQL_VERSION}
+DOWNLOAD_DIR_FULL=${DOWNLOAD_DIR}/${PRODUCT_SLUG}/${PCF_RABBIT_VERSION}
 mkdir  -p ${DOWNLOAD_DIR_FULL}
 
 curl \
@@ -41,13 +40,13 @@ curl \
 
 
 # download product using om cli
-echo $(date) start downloading MYSQL
+echo $(date) start downloading RABBIT
 om --skip-ssl-validation \
   download-product \
  --pivnet-api-token ${PCF_PIVNET_UAA_TOKEN} \
  --pivnet-file-glob "*.pivotal" \
  --pivnet-product-slug ${PRODUCT_SLUG} \
- --product-version ${PCF_MYSQL_VERSION} \
+ --product-version ${PCF_RABBIT_VERSION} \
  --stemcell-iaas azure \
  --download-stemcell \
  --output-directory ${DOWNLOAD_DIR_FULL}
@@ -83,42 +82,42 @@ om --skip-ssl-validation \
   --product-version ${VERSION}
 echo $(date) end staging ${PRODUCT_SLUG} 
 
-echo "creating storage account ${ENV_SHORT_NAME}mysqlbackup"
+echo "creating storage account ${ENV_SHORT_NAME}rabbitbackup"
 
 az login --service-principal \
   --username ${AZURE_CLIENT_ID} \
   --password ${AZURE_CLIENT_SECRET} \
   --tenant ${AZURE_TENANT_ID}
 
-az storage account create --name ${ENV_SHORT_NAME}mysqlbackup \
+az storage account create --name ${ENV_SHORT_NAME}rabbitbackup \
 --resource-group ${ENV_NAME} \
 --sku Standard_LRS \
 --location $LOCATION
 
-MYSQL_STORAGE_KEY=$(az storage account keys list \
---account-name ${ENV_SHORT_NAME}mysqlbackup \
+RABBIT_STORAGE_KEY=$(az storage account keys list \
+--account-name ${ENV_SHORT_NAME}rabbitbackup \
 --resource-group ${ENV_NAME} \
 --query "[0].{value:value}" \
 --output tsv
 )
 
 az storage container create --name backup \
---account-name ${ENV_SHORT_NAME}mysqlbackup \
---account-key ${MYSQL_STORAGE_KEY}
+--account-name ${ENV_SHORT_NAME}rabbitbackup \
+--account-key ${RABBIT_STORAGE_KEY}
 
-cat << EOF > ~/mysql_vars.yaml
+cat << EOF > ~/rabbit_vars.yaml
 product_name: ${PRODUCT_SLUG}
 pcf_pas_network: pcf-pas-subnet
 pcf_service_network: pcf-services-subnet
-azure_storage_access_key: ${MYSQL_STORAGE_KEY}
-azure_account: ${ENV_SHORT_NAME}mysqlbackup
+azure_storage_access_key: ${RABBIT_STORAGE_KEY}
+azure_account: ${ENV_SHORT_NAME}rabbitbackup
 global_recipient_email: ${PCF_NOTIFICATIONS_EMAIL}
 blob_store_base_url: blob.core.windows.net
 EOF
 
 om --skip-ssl-validation \
   configure-product \
-  -c ${HOME_DIR}/mysql.yaml -l ${HOME_DIR}/mysql_vars.yaml
+  -c rabbit.yaml -l rabbit_vars.yaml
 
 om --skip-ssl-validation \
 upload-stemcell \
