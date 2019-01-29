519215d6c3f17568c8dea3d3017b921029ad8e84
bottkars <karsten.bott@bottnet.de>
2019-01-29 13:13:00 +0100

    forced eula acceptance for 170 mand 97 stemcells

--- scripts/deploy_pas.sh
+++ scripts/deploy_pas.sh
@@ -81,6 +81,42 @@ PIVNET_ACCESS_TOKEN=$(curl \
   https://network.pivotal.io/api/v2/authentication/access_tokens |\
     jq -r '.access_token')
 
+
+## accept 170er stemcells
+RELEASE_JSON=$(curl \
+  --header "Authorization: Bearer ${PIVNET_ACCESS_TOKEN}" \
+  --fail \
+  "https://network.pivotal.io/api/v2/products/233/releases/286469")
+# eula acceptance link
+EULA_ACCEPTANCE_URL=$(echo ${RELEASE_JSON} |\
+  jq -r '._links.eula_acceptance.href')
+
+# eula acceptance
+curl \
+  --fail \
+  --header "Authorization: Bearer ${PIVNET_ACCESS_TOKEN}" \
+  --request POST \
+  ${EULA_ACCEPTANCE_URL}
+
+
+## accept 97er stemcells
+RELEASE_JSON=$(curl \
+  --header "Authorization: Bearer ${PIVNET_ACCESS_TOKEN}" \
+  --fail \
+  "https://network.pivotal.io/api/v2/products/233/releases/162133")
+# eula acceptance link
+EULA_ACCEPTANCE_URL=$(echo ${RELEASE_JSON} |\
+  jq -r '._links.eula_acceptance.href')
+
+# eula acceptance
+curl \
+  --fail \
+  --header "Authorization: Bearer ${PIVNET_ACCESS_TOKEN}" \
+  --request POST \
+  ${EULA_ACCEPTANCE_URL}
+
+
+
 # release by slug
 RELEASE_JSON=$(curl \
   --header "Authorization: Bearer ${PIVNET_ACCESS_TOKEN}" \
@@ -90,15 +126,17 @@ RELEASE_JSON=$(curl \
 EULA_ACCEPTANCE_URL=$(echo ${RELEASE_JSON} |\
   jq -r '._links.eula_acceptance.href')
 
-
-DOWNLOAD_DIR_FULL=${DOWNLOAD_DIR}/$PRODUCT_SLUG/${PCF_PAS_VERSION}-${PAS_EDITION}
-mkdir -p ${DOWNLOAD_DIR_FULL}
 # eula acceptance
 curl \
   --fail \
   --header "Authorization: Bearer ${PIVNET_ACCESS_TOKEN}" \
   --request POST \
   ${EULA_ACCEPTANCE_URL}
+
+
+DOWNLOAD_DIR_FULL=${DOWNLOAD_DIR}/$PRODUCT_SLUG/${PCF_PAS_VERSION}-${PAS_EDITION}
+mkdir -p ${DOWNLOAD_DIR_FULL}
+
 
 # download product using om cli
 if  [ -z ${NO_DOWNLOAD} ] ; then
