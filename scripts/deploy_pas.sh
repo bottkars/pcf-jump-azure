7bb59a7820fcf236319f1e6b3288d08f35c7589f
bottkars <karsten.bott@bottnet.de>
2019-03-30 21:39:21 +0100

    pas 25

--- scripts/deploy_pas.sh
+++ scripts/deploy_pas.sh
@@ -191,7 +191,7 @@ om --skip-ssl-validation \
 echo $(date) end staging ${PRODUCT_SLUG} 
 
 
-$SCRIPT_DIR/stemcell_loader.sh
+$SCRIPT_DIR/stemcell_loader.sh 250
 
 om --skip-ssl-validation \
 assign-stemcell \
