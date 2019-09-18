d3d629eaf536edc0c202d36f4e39056ba98eb914
bottkars <karsten.bott@bottnet.de>
2019-09-18 19:19:39 +0200

    latest w/o om

--- scripts/update.sh
+++ scripts/update.sh
@@ -12,9 +12,7 @@ if ! which parallel > /dev/null; then
    sudo apt install parallel -y
 fi   
 
-wget -O om https://github.com/pivotal-cf/om/releases/download/1.1.0/om-linux && \
-    chmod +x om && \
-    sudo mv om /usr/local/bin/
+
 
 echo "Preparing Updates"
 declare -a DIRECTORIES=("scripts" "env" "templates")
