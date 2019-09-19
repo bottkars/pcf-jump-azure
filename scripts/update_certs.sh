c4408f8f3c1d3ac649b5d1c8e8fa32f0f4dd0b91
bottkars <karsten.bott@bottnet.de>
2019-09-19 08:13:50 +0200

    update certs

--- scripts/update_certs.sh
+++ scripts/update_certs.sh
@@ -4,5 +4,27 @@ update-ssl-certificate \
     --certificate-pem "$(cat ${HOME_DIR}/fullchain.cer)" \
     --private-key-pem "$(cat ${HOME_DIR}/${PCF_SUBDOMAIN_NAME}.${PCF_DOMAIN_NAME}.key)"
 
+
+
+
+cat << EOF > ${TEMPLATE_DIR}/director_certs_vars.yml
+fullchain: "$(cat ${HOME_DIR}/fullchain.cer | awk '{printf "%s\\r\\n", $0}')"
+EOF
+
+om --env "${HOME_DIR}/om_${ENV_NAME}.env"  \
+ configure-director --config ${TEMPLATE_DIR}/director_certs.yml --vars-file ${TEMPLATE_DIR}/director_certs_vars.yml
+
+
+
 PCF_KEY_PEM=$(cat ${HOME_DIR}/${PCF_SUBDOMAIN_NAME}.${PCF_DOMAIN_NAME}.key | awk '{printf "%s\\r\\n", $0}')
 PCF_CERT_PEM=$(cat ${HOME_DIR}/fullchain.cer | awk '{printf "%s\\r\\n", $0}')
+
+
+cat << EOF > ${TEMPLATE_DIR}/pas_cert_vars.yml
+pcf_cert_pem: "${PCF_CERT_PEM}"
+pcf_key_pem: "${PCF_KEY_PEM}"
+EOF
+
+om --env "${HOME_DIR}/om_${ENV_NAME}.env"  \
+  configure-product \
+  -c ${TEMPLATE_DIR}/pas_cert.yml -l ${TEMPLATE_DIR}/pas_cert_vars.yml
