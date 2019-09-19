141fdb882246f83f35f521e37b015e17f12b1bc9
bottkars <karsten.bott@bottnet.de>
2019-09-19 08:17:42 +0200

    added certs updatrer

--- scripts/update_certs.sh
+++ scripts/update_certs.sh
@@ -1,18 +1,20 @@
-
+#!/usr/bin/env bash
+source ~/.env.sh
+MYSELF=$(basename $0)
+echo "this is the certs updater"
 om --env "${HOME_DIR}/om_${ENV_NAME}.env"  \
 update-ssl-certificate \
     --certificate-pem "$(cat ${HOME_DIR}/fullchain.cer)" \
     --private-key-pem "$(cat ${HOME_DIR}/${PCF_SUBDOMAIN_NAME}.${PCF_DOMAIN_NAME}.key)"
-
-
-
 
 cat << EOF > ${TEMPLATE_DIR}/director_certs_vars.yml
 fullchain: "$(cat ${HOME_DIR}/fullchain.cer | awk '{printf "%s\\r\\n", $0}')"
 EOF
 
 om --env "${HOME_DIR}/om_${ENV_NAME}.env"  \
- configure-director --config ${TEMPLATE_DIR}/director_certs.yml --vars-file ${TEMPLATE_DIR}/director_certs_vars.yml
+   configure-director \
+   --config ${TEMPLATE_DIR}/director_certs.yml \
+   --vars-file ${TEMPLATE_DIR}/director_certs_vars.yml
 
 
 
@@ -20,11 +22,12 @@ PCF_KEY_PEM=$(cat ${HOME_DIR}/${PCF_SUBDOMAIN_NAME}.${PCF_DOMAIN_NAME}.key | awk
 PCF_CERT_PEM=$(cat ${HOME_DIR}/fullchain.cer | awk '{printf "%s\\r\\n", $0}')
 
 
-cat << EOF > ${TEMPLATE_DIR}/pas_cert_vars.yml
+cat << EOF > ${TEMPLATE_DIR}/pas_certs_vars.yml
 pcf_cert_pem: "${PCF_CERT_PEM}"
 pcf_key_pem: "${PCF_KEY_PEM}"
 EOF
 
 om --env "${HOME_DIR}/om_${ENV_NAME}.env"  \
   configure-product \
-  -c ${TEMPLATE_DIR}/pas_cert.yml -l ${TEMPLATE_DIR}/pas_cert_vars.yml
+  -c ${TEMPLATE_DIR}/pas_certs.yml \
+  -l ${TEMPLATE_DIR}/pas_certs_vars.yml
